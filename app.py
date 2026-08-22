9cae43eb59a36c0b36b69a0f9532d0377813ce4f
Rebbapragada Kartheek <23f2000229@ds.study.iitm.ac.in>
2026-08-22 09:59:06 +0530

    Test Run

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-"Hi"
+from flask import Flask, render_template,request
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import (
+    LoginManager,
+    UserMixin,
+    login_user,
+    logout_user,
+    login_required,
+    current_user
+)
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return "Hello, Flask!"
+
+if __name__ == '__main__':
+    app.run(debug=True)

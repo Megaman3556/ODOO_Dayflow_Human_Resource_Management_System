a9381949667c874073f67f49fe7d1fad38974308
Rebbapragada Kartheek <23f2000229@ds.study.iitm.ac.in>
2026-08-22 11:17:38 +0530

    Authentication Done

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
     LoginManager,
@@ -31,13 +31,70 @@ class User(db.Model):
     role = db.Column(db.String(20), nullable=False)  # employee / hr
     password = db.Column(db.String(255), nullable=False)
     
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def register():
-    return render_template("account.html")
+    if request.method == "GET":
+        return render_template('register.html')
 
+    else:
+        company_name = request.form.get("company")
+        employee_id = request.form.get("employee_id")
+        name = request.form.get("name")
+        email = request.form.get("email")
+        phone = request.form.get("phone")
+        role = request.form.get("role")
+        password = request.form.get("password")
+        confirm_password = request.form.get("confirm-password")
 
-@app.route("/login")
-def login():
+        if password != confirm_password:
+            return "Passwords do not match"
+
+        person=User(
+            company_name=company_name,
+            employee_id=employee_id,
+            name=name,
+            email=email,
+            phone=phone,
+            role=role,
+            password=password
+        )
+
+        db.session.add(person)
+        db.session.commit()
     return render_template("login.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+
+    if request.method == "GET":
+        return render_template("login.html")
+
+    email = request.form.get("email")
+    password = request.form.get("password")
+    role = request.form.get("role")
+
+    user = User.query.filter_by(
+        email=email,
+        password=password,
+        role=role
+    ).first()
+
+    if user:
+        return redirect(url_for("user", employee_id=user.employee_id))
+
+    return "Invalid email, password, or role"
+
+
+@app.route("/user/<employee_id>")
+def user(employee_id):
+
+    user = User.query.filter_by(employee_id=employee_id).first()
+
+    if user:
+        return render_template("user.html", user=user)
+
+    return "User not found"
+
 if __name__ == '__main__':
     app.run(debug=True)

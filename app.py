33b74326c94437126f90db61b69991c2266bb6ee
Rebbapragada Kartheek <23f2000229@ds.study.iitm.ac.in>
2026-08-22 14:55:49 +0530

    Almost all user functionalities added

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template,request,redirect, url_for
+from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import (
     LoginManager,
@@ -16,7 +17,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///company.sqlite3"
 
 db = SQLAlchemy(app) 
 
-# once you run from app import app,db in shell (python) run type the below
 
 app.app_context().push()
 
@@ -30,6 +30,42 @@ class User(db.Model):
     phone = db.Column(db.String(20), nullable=False)
     role = db.Column(db.String(20), nullable=False)  # employee / hr
     password = db.Column(db.String(255), nullable=False)
+
+class Leave(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable=False
+    )
+
+    leave_type = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    from_date = db.Column(
+        db.Date,
+        nullable=False
+    )
+
+    to_date = db.Column(
+        db.Date,
+        nullable=False
+    )
+
+    remarks = db.Column(
+        db.Text,
+        nullable=True
+    )
+
+    status = db.Column(
+        db.String(20),
+        default="Pending",
+        nullable=False
+    )
 
 class EmployeeProfile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -54,7 +90,67 @@ class EmployeeProfile(db.Model):
     reporting_manager = db.Column(db.String(100))
 
     profile_picture = db.Column(db.String(255))
-    
+
+class Salary(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey("user.id"),
+        nullable=False,
+        unique=True
+    )
+
+    # Cost to Company
+    cost_to_company_annual = db.Column(db.Float)
+    cost_to_company_monthly = db.Column(db.Float)
+
+    # Salary Components
+    basic_annual = db.Column(db.Float)
+    basic_monthly = db.Column(db.Float)
+
+    hra_annual = db.Column(db.Float)
+    hra_monthly = db.Column(db.Float)
+
+    medical_annual = db.Column(db.Float)
+    medical_monthly = db.Column(db.Float)
+
+    special_allowance_annual = db.Column(db.Float)
+    special_allowance_monthly = db.Column(db.Float)
+
+    telephone_annual = db.Column(db.Float)
+    telephone_monthly = db.Column(db.Float)
+
+    conveyance_annual = db.Column(db.Float)
+    conveyance_monthly = db.Column(db.Float)
+
+    ex_gratia_annual = db.Column(db.Float)
+    ex_gratia_monthly = db.Column(db.Float)
+
+    # Employer contribution
+    pf_employer_annual = db.Column(db.Float)
+    pf_employer_monthly = db.Column(db.Float)
+
+    # Gross salary
+    total_annual = db.Column(db.Float)
+    total_monthly = db.Column(db.Float)
+
+    # Employee deductions
+    pf_employee_annual = db.Column(db.Float)
+    pf_employee_monthly = db.Column(db.Float)
+
+    esi_annual = db.Column(db.Float)
+    esi_monthly = db.Column(db.Float)
+
+    labor_welfare_annual = db.Column(db.Float)
+    labor_welfare_monthly = db.Column(db.Float)
+
+    # Take home
+    net_salary_annual = db.Column(db.Float)
+    net_salary_monthly = db.Column(db.Float)
+
+    user = db.relationship("User", backref="salary")
+
 @app.route('/',methods=["GET","POST"])
 def register():
     if request.method == "GET":
@@ -109,7 +205,6 @@ def login():
 
         return "Invalid"
 
-
 @app.route("/user/<employee_id>")
 def user(employee_id):
 
@@ -120,9 +215,80 @@ def user(employee_id):
 
     return "User not found"
 
-@app.route("/profile<employee_id>")
-def profile():
-    pass
+@app.route("/profile/<emp_id>")
+def profile(emp_id):
+    user=User.query.filter_by(employee_id=emp_id).first()
+    user_id = user.id
+    emp_profile=EmployeeProfile.query.filter_by(user_id=user_id).first()
+    salary=Salary.query.filter_by(user_id=user_id).first()
+    return render_template("profile.html",emp_profile=emp_profile,user=user,salary=salary)
 
+@app.route("/profile/edit/<emp_id>", methods=["POST"])
+def emp_profile_edit(emp_id):
+
+    user = User.query.filter_by(employee_id=emp_id).first()
+
+    if not user:
+        return "User not found"
+
+    emp_profile = EmployeeProfile.query.filter_by(
+        user_id=user.id
+    ).first()
+
+    new_address = request.form.get("address")
+    new_phone = request.form.get("phone")
+
+    # Update phone
+    user.phone = new_phone
+
+    # Update address
+    emp_profile.address = new_address
+
+    db.session.commit()
+
+    return redirect("/profile/" + user.employee_id)
+
+@app.route("/leave/<emp_id>", methods=["GET", "POST"])
+def emp_leave_request(emp_id):
+
+    user = User.query.filter_by(employee_id=emp_id).first()
+
+    if not user:
+        return "User not found"
+
+    if request.method == "GET":
+        return render_template(
+            "leave_request.html",
+            user=user
+        )
+
+    else:
+        leave_type = request.form.get("leave_type")
+        from_date = request.form.get("from_date")
+        to_date = request.form.get("to_date")
+        remarks = request.form.get("remarks")
+
+        from_date = datetime.strptime(from_date, "%Y-%m-%d").date()
+        to_date = datetime.strptime(to_date, "%Y-%m-%d").date()
+
+        leave = Leave(
+            user_id=user.id,
+            leave_type=leave_type,
+            from_date=from_date,
+            to_date=to_date,
+            remarks=remarks
+        )
+
+        db.session.add(leave)
+        db.session.commit()
+
+        return "Leave request submitted successfully"
+
+@app.route("/payroll/<emp_id>")
+def emp_payroll(emp_id):
+    user=User.query.filter_by(employee_id=emp_id).first()
+    user_id=user.id
+    salary_details=Salary.query.filter_by(user_id=user_id).first()
+    return render_template("payroll.html",salary=salary_details)
 if __name__ == '__main__':
     app.run(debug=True)
